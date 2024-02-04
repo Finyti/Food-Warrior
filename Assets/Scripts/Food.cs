1158966805d154c30e53c4a3e9deac6099e3189e
Finyti <122672171+Finyti@users.noreply.github.com>
2024-02-04 19:16:43 +0200

    Particle systems and improvements

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -8,31 +8,49 @@ public class Food : MonoBehaviour
     private Rigidbody2D rb;
 
     public Vector2 startSpeed;
+    public float rotateSpeed;
 
     private bool isSlicing = false;
+
+    public GameObject fruitExplode;
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = startSpeed;
+        rb.angularVelocity = Random.Range(-rotateSpeed, rotateSpeed);
     }
 
     void Update()
     {
         if(transform.position.y < -6)
         {
-            FruitLoose();
-            Destroy(gameObject);
+            Miss();
         }
+    }
+
+
+
+    private void OnMouseOver()
+    {
+        Slice();
+    }
+
+    private void Miss()
+    {
+        FruitLoose();
+        Destroy(gameObject);
+    }
+    private void Slice()
+    {
+        GameObject particles = Instantiate(fruitExplode, transform.position, Quaternion.identity);
+        Destroy(gameObject);
     }
 
     void FruitLoose()
     {
 
-    }
-
-    private void OnMouseOver()
-    {
-        print("FFF");
     }
 
 }
--- Assets/Scripts/FruitsManager.cs
+++ Assets/Scripts/FruitsManager.cs
@@ -9,8 +9,14 @@ public class FruitsManager : MonoBehaviour
 {
 
     public List<GameObject> prefabs = new List<GameObject>();
+    public GameObject bombPrefab;
 
     public float spawnSpeed = 1f;
+
+    public float bombChance = 20;
+
+    public GameObject bombSmoke;
+    public GameObject bombSparkles;
     void Start()
     {
         Task testTask1 = AsyncTimer.Delay(spawnSpeed, CreateFruit, false);
@@ -24,9 +30,24 @@ public class FruitsManager : MonoBehaviour
     {
         if(Application.isPlaying)
         {
-            var r = Random.Range(0, prefabs.Count);
+            bool isBomb = Random.Range(0, 100) < bombChance;
+            GameObject fruit;
+            if (isBomb)
+            {
+                fruit = Instantiate(bombPrefab, bombPrefab.transform.position, bombPrefab.transform.rotation);
+                GameObject smoke = Instantiate(bombSmoke, fruit.transform.position, fruit.transform.rotation);
+                GameObject sparkles = Instantiate(bombSparkles, fruit.transform.position, fruit.transform.rotation);
+                smoke.transform.parent = fruit.transform;
+                sparkles.transform.parent = fruit.transform;
+                smoke.transform.position += new Vector3(0, 0.8f, 0);
+                sparkles.transform.position += new Vector3(0, 0.8f, 0);
+            }
+            else
+            {
+                var r = Random.Range(0, prefabs.Count);
+                fruit = Instantiate(prefabs[r], prefabs[r].transform.position, prefabs[r].transform.rotation);
+            }
 
-            GameObject fruit = Instantiate(prefabs[r], prefabs[r].transform.position, prefabs[r].transform.rotation);
             fruit.transform.position += new Vector3(Random.Range(-6, 6), 0, 0);
 
             int minXPower = -3;
